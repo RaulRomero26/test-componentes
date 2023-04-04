0e158ae12b80417dc65164c54608928a1b625704
RaulRomero26 <link_2642@hotmail.com>
2023-04-04 16:06:23 -0600

    componente de descarga de pdf

--- src/components/PDF/MyPdf.jsx
+++ src/components/PDF/MyPdf.jsx
@@ -1,29 +1,80 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import { PDFDownloadLink, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet, Font,Image } from '@react-pdf/renderer';
+
+Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+});
 
 const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+  body: {
+    paddingTop: 35,
+    paddingBottom: 65,
+    paddingHorizontal: 35,
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
-  }
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontFamily: 'Oswald'
+  },
+  pageNumber: {
+    position: 'absolute',
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+    color: 'grey',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexGrow: 1,
+  },
+  left: {
+    width: '40%',//<- working alternative
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 300,
+  },
+
+  right: {
+    padding: 5,
+    width: '60%', //<- working alternative
+    flexShrink: 1,
+    flexGrow: 2,
+  },
 });
 
 const MyDoc = () => (
   <Document>
-    <Page size="Letter" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
+    <Page style={styles.body} orientation="landscape">
+       <Text >
+      	Titulo
+      	</Text>
+      
+        <View style={[styles.row]}>
+        	<View  style={styles.left}>
+          		<Image
+        			style={styles.image}
+        			src={{ uri:'http://172.18.0.25/sarai/public/files/Remisiones/19863/FotosHuellas/264019/rostro_frente.jpeg', method: "GET", headers: { "Cache-Control": "no-cache" }, body: "" }} 
+      			/>
+        	</View>
+        	<View debug style={styles.right}>
+          		<Text>
+            	datos
+          		</Text>
+              	<Text>
+            	nombre
+          		</Text>
+        	</View>
+      	</View>
+      
+      <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
+        `${pageNumber} / ${totalPages}`
+      )} fixed />
     </Page>
   </Document>
 );
@@ -32,9 +83,10 @@ const MyDoc = () => (
 export const MyPdf = () => {
   return (
     <div className="App">
-      <PDFDownloadLink document={<MyDoc />} fileName="somename.pdf">
+      {/* <PDFDownloadLink document={<MyDoc />} fileName="somename.pdf">
       {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-    </PDFDownloadLink>
+    </PDFDownloadLink> */}
+   
     </div>
   );
 }
